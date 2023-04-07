9f68e3c1a036a0ff925c3be616c08b4028f3d37b
alifiavidia <102420750+alifiavidia@users.noreply.github.com>
2023-04-07 11:23:24 +0700

    Update PSO.py

--- optimizers/PSO.py
+++ optimizers/PSO.py
@@ -66,7 +66,7 @@ def PSO(objf, lb, ub, dim, PopSize, iters):
             if gBestScore > fitness:
                 gBestScore = fitness
                 gBest = pos[i, :].copy()
-                print(gbest)
+                print(gBest)
 
         # Update the W of PSO
         w = wMax - l * ((wMax - wMin) / iters)
